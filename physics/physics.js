ca2628cd77ea2847a0a1e116e1ac817a4f42f8e3
Jonathan Lin <jlin17@stu.hpreg.org>
2017-05-11 12:42:59 -0400

    Update physics.js

--- physics/physics.js
+++ physics/physics.js
@@ -1,3 +1,25 @@
 Simulation = {
 
 }
+
+Scene = {
+  balls: [],
+  ramps: []
+}
+
+Ball = {
+  x: 0,
+  y: 0,
+  dX: 0,
+  dY: 0,
+  randomPos: function() {
+    this.x = getRandomInt(0, 590);
+    this.y = getRandomInt(0, 270);
+  }
+}
+  
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
